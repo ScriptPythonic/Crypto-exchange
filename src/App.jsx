857f8d2a82616238bc0e6a583df78f3f30d23ffa
scriptpythonic <Quadribasit04@gmail.com>
2025-01-12 00:59:46 +0100

    added new update

--- src/App.jsx
+++ src/App.jsx
@@ -24,21 +24,21 @@ function App (){
 
       </div>
       {/* end of the main part */}
-      <div className='flex-1 max-w-[500px]'>
-  <div className='flex gap-6 items-center py-4 mx-auto'>
-    <FaRegUserCircle className='text-3xl text-white' />
-    <div className='flex items-center bg-[#9aa6be] rounded-[25px] pl-2 gap-3'>
-      <div className='flex items-center gap-[4px]'>
-        <div className='w-2 h-2 bg-red-600 rounded-full'></div>
-        <p>BSC</p>
+      <div className='lg:w-[500px] w-0'>
+        <div className='mx-auto flex gap-6 items-center py-4 w-fit'>
+          <FaRegUserCircle className='text-3xl text-white' />
+          <div className='flex items-center bg-[#9aa6be] rounded-[25px] pl-2 gap-3'>
+            <div className='flex items-center gap-[4px]'>
+              <div className='w-2 h-2 bg-red-600 rounded-full'></div>
+              <p>BSC</p>
+            </div>
+            <button className='px-3 py-2 bg-[#E33319] rounded-[25px]'>Connect to a wallet</button>
+          </div>
+          <div className='w-9 h-9 bg-[#9aa6be] rounded-[4px] flex justify-center items-center'>
+            <div className='w-5 h-5 bg-white rounded-full'></div>
+          </div>
+        </div>
       </div>
-      <button className='px-3 py-2 bg-[#E33319] rounded-[25px]'>Connect to a wallet</button>
-    </div>
-    <div className='w-9 h-9 bg-[#9aa6be] rounded-[4px] flex justify-center items-center'>
-      <div className='w-5 h-5 bg-white rounded-full'></div>
-    </div>
-  </div>
-</div>
 
     </div>
   )
