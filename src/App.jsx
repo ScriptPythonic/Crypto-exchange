4a02705aeb3f029cad58b5f90f5cedb80f1c62ed
scriptpythonic <Quadribasit04@gmail.com>
2025-01-12 02:02:38 +0100

    Added the graph

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './app.css'
 import Logo from './assets/image.png'
 import { FaRegUserCircle } from "react-icons/fa";
+import coin2x from './assets/coin@2x.svg'
+import Arrow from './assets/arrow-exchange-swap.png'
+import CryptoChart from './components/chart';
 
 function App (){
   return (
@@ -20,6 +23,27 @@ function App (){
             <a href="#">Developer</a>
           </div>
           
+        </div>
+
+        <div className=' mt-12 px-6'>
+          <div className='flex items-center gap-1'>
+            <img src={coin2x} alt="coin for crypto"/>
+            <h2 className='text-xl font-bold'>BUSD/<span className='text-gray-500'>USDC</span></h2>
+            <img src={Arrow} alt="arrow " width={25} height={25} />
+          </div>
+          <div className='mt-5'>
+            <h2 className='text-4xl font-bold'>1.005 USDC</h2>
+            <div className='flex justify-between'>
+              <p className='text-[#53F3C3]'>+227.543364 USDC <span className='text-white'>Past 24 Hours</span></p>
+              <div className='flex gap-5'>
+                <div className='bg-[#3B3C4E] px-2 py-1 rounded-lg ring-1'><p>24H</p></div>
+                <div className='bg-[#3B3C4E] px-2 py-1 rounded-lg'><p>1W</p></div>
+                <div className='bg-[#3B3C4E] px-2 py-1 rounded-lg'><p>1M</p></div>
+              </div>
+            </div>
+          </div>
+
+          <CryptoChart className="mt-6"/>
         </div>
 
       </div>
