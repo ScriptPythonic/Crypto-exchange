d8819f789d766e0ec10eba47d6d0da3d8ddb65b0
scriptpythonic <Quadribasit04@gmail.com>
2025-01-12 12:39:36 +0100

    added crypto payment gateway

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,24 @@
 import PaymentForm from "./PaymentForm"
 import { FaRegUserCircle } from "react-icons/fa";
-
+import { client } from './hooks/client';
+import { ConnectButton } from "thirdweb/react";
 
 function Sidebar(){
    return(
       <div className='lg:w-[500px] lg:block hidden bg-gray-800'>
         <div className='mx-auto flex gap-6 items-center py-4 w-fit'>
           <FaRegUserCircle className='text-3xl text-white' />
-          <div className='flex items-center bg-[#9aa6be] rounded-[25px] pl-2 gap-3'>
+          <div className='flex items-center bg-gray-600 rounded-[25px] pl-2 gap-3'>
             <div className='flex items-center gap-[4px]'>
               <div className='w-2 h-2 bg-red-600 rounded-full'></div>
               <p>BSC</p>
             </div>
-            <button className='px-3 py-2 bg-[#E33319] rounded-[25px]'>Connect to a wallet</button>
+            <div>
+            <ConnectButton client={client} />
+            </div>
+            
           </div>
-          <div className='w-9 h-9 bg-[#9aa6be] rounded-[4px] flex justify-center items-center'>
+          <div className='w-9 h-9 bg-gray-600 rounded-[4px] flex justify-center items-center'>
             <div className='w-5 h-5 bg-white rounded-full'></div>
           </div>
         </div>
--- src/components/hooks/client.jsx
+++ src/components/hooks/client.jsx
@@ -1,5 +1,5 @@
 import { createThirdwebClient } from "thirdweb";
  
 export const client = createThirdwebClient({
-  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
+  clientId: 'bdb07a03e06e143cab8ce5ea9520b8a8',
 });
